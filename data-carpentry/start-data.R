1fcd410e5988a98d6e22b97099948819754f5827
Aleksandra Wilczynska <a.e.wilczynska@tudelft.nl>
2025-10-08 15:35:06 +0200

    gitautosync update 13

--- data-carpentry/start-data.R
+++ data-carpentry/start-data.R
@@ -2,4 +2,5 @@ library(tidyverse)
 # install.packages("here") <- if you don't have it 
 library(here)
 
-interviews <- read_csv(here("data", "SAFI_clean.csv"), na = "NULL")
+interviews <- read_csv(here("data", "SAFI_clean.csv"), 
+                       na = "NULL")
