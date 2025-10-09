9d58f968a8b2797f0cd8f687f552cfd53b39f0a1
peterverhaar <peter.verhaar@gmail.com>
2025-10-09 09:30:20 +0200

    gitautosync update 5

--- data-carpentry/scripts/data_wrangling.R
+++ data-carpentry/scripts/data_wrangling.R
@@ -7,3 +7,14 @@ interviews <- read_csv( here("data","SAFI_clean.csv") , na = "NULL")
 
 # select the village column
 select( interviews , village )
+
+# select multiple columns
+select( interviews , village , no_membrs , interview_date)
+
+# select range of columns
+select( interviews, village:respondent_wall_type )
+
+# select all columns staring with 'no_'
+select( interviews, starts_with('no_') )
+
+interviews2 <- select( interviews, starts_with('no_') )
