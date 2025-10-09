575448c68d8b3abc5c47b8668e05a108d43e4fb9
peterverhaar <peter.verhaar@gmail.com>
2025-10-09 11:34:06 +0200

    gitautosync update 18

--- data-carpentry/scripts/data_wrangling.R
+++ data-carpentry/scripts/data_wrangling.R
@@ -93,3 +93,24 @@ wide_date <- pivot_wider( long_data ,
 transposed_interviews <- as_tibble( t(interviews) )
 
 write_csv(interviews2, here( "data","changed_data.csv" ) )
+
+
+## Plotting data ##
+interviews_plotting <- interviews %>%
+  ## pivot wider by items_owned
+  separate_longer_delim(items_owned, delim = ";") %>%
+  replace_na(list(items_owned = "no_listed_items")) %>%
+  ## Use of grouped mutate to find number of rows
+  group_by(key_ID) %>% 
+  mutate(items_owned_logical = TRUE,
+         number_items = if_else(items_owned == "no_listed_items", 0, n())) %>% 
+  pivot_wider(names_from = items_owned,
+              values_from = items_owned_logical,
+              values_fill = list(items_owned_logical = FALSE)) %>% 
+  ## pivot wider by months_lack_food
+  separate_longer_delim(months_lack_food, delim = ";") %>%
+  mutate(months_lack_food_logical = TRUE,
+         number_months_lack_food = if_else(months_lack_food == "none", 0, n())) %>%
+  pivot_wider(names_from = months_lack_food,
+              values_from = months_lack_food_logical,
+              values_fill = list(months_lack_food_logical = FALSE))
