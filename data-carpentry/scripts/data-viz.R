82f150fa107c80e78c8d663a0523ddd33bf7a096
bbartholdy <47455812+bbartholdy@users.noreply.github.com>
2025-10-09 13:37:44 +0200

    gitautosync update 1

---
+++ data-carpentry/scripts/data-viz.R
@@ -0,0 +1,50 @@
+library(tidyverse)
+
+interviews_plotting <- read_csv("data/interviews_plotting.csv")
+interviews_plotting <- read_csv("data_output/interviews_plotting.csv")
+
+interviews_plotting %>%
+  ggplot(mapping = aes(x = no_membrs, y = number_items)) +
+  geom_point(alpha = 0.2)
+
+interviews_plotting %>%
+  ggplot(mapping = aes(x = no_membrs, y = number_items)) +
+  geom_jitter(
+    width = 0.2,
+    height = 0.2,
+    alpha = 0.5,
+    colour = "tomato"
+  )
+
+interviews_plotting %>%
+  ggplot(aes(x = no_membrs, y = number_items)) +
+  geom_jitter(
+    aes(colour = village),
+    width = 0.2,
+    height = 0.2,
+    alpha = 0.5
+  ) +
+  geom_smooth(method = "lm")
+
+
+ggplot(mapping = aes(x = no_membrs, y = number_items, colour = village), data = interviews_plotting) +
+  geom_count()
+ggplot()
+
+## Exercise 1
+interviews_plotting %>%
+  ggplot(aes(x = village, y = rooms, colour = respondent_wall_type)) +
+  geom_jitter()
+
+
+
+
+
+
+
+
+
+
+
+
+
