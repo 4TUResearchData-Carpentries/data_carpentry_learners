a40fe968b0bfa8b18f5a701c3a360e48b4ca0c9e
Narmin Rzayeva <ndrrzayeva@tudelft.nl>
2026-03-26 09:32:21 +0100

    gitautosync update 3

--- data-carpentry/scripts/data_wrangling.R
+++ data-carpentry/scripts/data_wrangling.R
@@ -1,2 +1,9 @@
 ##Data wrangling 
-
+##load the packages
+library(tidyverse)
+library(here)
+##read the data
+interviews<-read_csv(here("data","SAFI_clean.csv"),na="NULL")
+##we select specific columns
+select(interviews,village)
+select(interviews,village,no_membrs, rooms)
