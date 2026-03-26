282bad0f2649175aa38afd3b118829522bdd7cd8
Narmin Rzayeva <ndrrzayeva@tudelft.nl>
2026-03-26 09:48:57 +0100

    gitautosync update 6

--- data-carpentry/scripts/data_wrangling.R
+++ data-carpentry/scripts/data_wrangling.R
@@ -12,4 +12,8 @@ interviews2<-select(interviews,village:memb_assoc)
 interviews3<-select(interviews, starts_with("no_")) 
 view(interviews3)
 ##Filtering
-interviews4<-filter(interviews, )
+interviews4<-filter(interviews, village=="Ruaca")
+##test for multiple criteria
+interviews4_1<-filter(interviews,village=="Ruaca" & no_membrs>5)
+##selection and filtering at the same time
+##fiter observations for Ruaca village but to return in result columns from village to rooms
