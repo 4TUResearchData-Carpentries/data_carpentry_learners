92c138a4c3b673d93182e8ab3ff82f3d64435186
Narmin Rzayeva <ndrrzayeva@tudelft.nl>
2026-03-26 09:41:41 +0100

    gitautosync update 5

--- data-carpentry/scripts/data_wrangling.R
+++ data-carpentry/scripts/data_wrangling.R
@@ -8,3 +8,8 @@ interviews<-read_csv(here("data","SAFI_clean.csv"),na="NULL")
 select(interviews,village)
 select(interviews,village,no_membrs, rooms)
 interviews2<-select(interviews,village:memb_assoc)
+##select columns with a certain pattern
+interviews3<-select(interviews, starts_with("no_")) 
+view(interviews3)
+##Filtering
+interviews4<-filter(interviews, )
